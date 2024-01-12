77384f851b767494b091451928826d0e7cc31ff1
Anusha-chitrala1 <153604490+Anusha-chitrala1@users.noreply.github.com>
2024-01-12 14:03:01 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './Components/navbar/Navbar'
+import News from './Components/navbar/newss/News'
+
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <News/>
+    </div>
+  )
+}
+
+export default App
+
+
+
